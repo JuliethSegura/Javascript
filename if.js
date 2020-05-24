91ebe610a22f56c1780b44aa6418efdd687c9756
Julieth Segura <ju.segura@udla.edu.co>
2020-05-23 23:55:37 -0500

    Segunda parte clases

---
+++ if.js
@@ -0,0 +1,15 @@
+var edad = 18;
+
+if (edad === 18) {
+    console.log("Puedes votar")
+} else if (edad > 18) {
+    console.log("Puedes votar de nuevo");
+} else {
+    console.log("Aún no puedes votar")
+}
+
+
+condition ? true : false;
+
+var numero = 1;
+var resultado = numero === 1 ? "Sí soy un uno" : "No, no soy uno"
---
+++ operadores.js
@@ -0,0 +1,30 @@
+// Operadores binarios
+3 + 2
+50 - 10
+10 * 3
+20 / 2
+
+    "Julieth" + "De granda"
+
+//Operadores unitarios
+!false
+
+//Asignación
+var a = 1;
+
+//Operadores de Comparación
+3 == "3" //es igual
+3 === "3" //es estrictamente igual
+5 < 3, 5 <= 3, 5 > 3, 5 >= 3 // menor, menor o igual, mayo, mayor o igual 
+
+//Operadores logicos
+a && b // operador and (y), este operador genera una valicación siempe y cuando ambas variables sean verdad, "var 1 y var 2"
+true || false // operador or (o), este operador genera una validacion siempre y cuando cuaquier de as variables sea verdad, "var 1 o var 2"
+
+//Operadores de incremento
+var edad = 40;
+
+edad++; // operador de incremento por 1
+edad += 2;
+
+edad;
